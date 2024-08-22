5850cab163f3bc2e662809d85e21622d9a9c8e9d
bateman001 <49342979+bateman001@users.noreply.github.com>
2024-08-22 12:35:07 -0400

    added storybook for development

---
+++ src/components/Button/Button.stories.tsx
@@ -0,0 +1,25 @@
+import { Meta, StoryObj } from "@storybook/react";
+import Button from "./Button";
+
+// Export metadata with title
+const meta: Meta<typeof Button> = {
+    title: "SampleComponentLibrary/Button",
+    component: Button
+};
+export default meta;
+
+// create reusable Story type
+type Story = StoryObj<typeof Button>;
+
+// export story
+export const Primary: Story = {
+    args: {
+        label: "Primary"
+    }
+};
+
+export const Secondary: Story = {
+    args: {
+        label: "Secondary"
+    }
+};
